5f0da8ee5804ec125c8e3d313cdedcd11ff938d2
mutoe <mutoe@foxmail.com>
2020-01-03 19:14:00 +0800

    test: fix service test errors

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UserService } from './user.service'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { User } from './user.entity'
+import { Repository } from 'typeorm'
 
 describe('UserService', () => {
   let service: UserService
+  let repository: Repository<User>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService, {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile()
 
     service = module.get(UserService)
+    repository = module.get(getRepositoryToken(User))
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+    expect(repository).toBeDefined()
   })
 })
