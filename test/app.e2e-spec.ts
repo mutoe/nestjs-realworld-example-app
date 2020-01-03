65074d0d100bc7d7394feacb6ec1805d6c4913a6
mutoe <mutoe@foxmail.com>
2020-01-03 21:34:51 +0800

    test: add e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import * as request from 'supertest'
 import { AppModule } from '../src/app.module'
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
+import { getConnection } from 'typeorm'
+
+const ormConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'realworld',
+  password: '123456',
+  database: 'nestjs_test',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  dropSchema: true,
+  synchronize: true,
+}
 
 describe('AppController (e2e)', () => {
   let app
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        TypeOrmModule.forRoot(ormConfig),
+        AppModule,
+      ],
     }).compile()
 
     app = moduleFixture.createNestApplication()
     await app.init()
+  })
+
+  afterEach(async () => {
+    await app.close()
   })
 
   it('/hello (GET)', () => {
@@ -19,5 +40,17 @@ describe('AppController (e2e)', () => {
       .get('/hello?name=world')
       .expect(200)
       .expect('Hello world!')
+  })
+
+  it('/auth/register (POST)', async () => {
+    const requestBody = {
+      username: 'mutoe',
+      email: 'mutoe@foxmail.com',
+      password: '12345678',
+    }
+    await request(app.getHttpServer())
+      .post('/auth/register')
+      .send(requestBody)
+      .expect(201)
   })
 })
