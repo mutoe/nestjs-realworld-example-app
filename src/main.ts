10002adf9e4dab738555ea3ad66dba36906a17d1
mutoe <mutoe@foxmail.com>
2020-05-02 17:22:06 +0800

    chore: add swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
 import { AppModule } from './app.module'
+
+function createSwagger (app: INestApplication) {
+  const version = require('../package.json').version || ''
+
+  const options = new DocumentBuilder()
+    .setTitle('Nestjs Realworld Example App')
+    .setVersion(version)
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('/docs', app, document)
+}
 
 async function bootstrap () {
   const app = await NestFactory.create(AppModule)
   await app.listen(3000)
 }
 
-bootstrap()
+bootstrap().catch(err => console.error(err))
